bae4533ca2b6b1985a308511e46c79df82ff5154
Maël NACCACHE <mael.naccache@zenika.com>
2018-02-07 16:45:20 +0100

    :hankey: Trying to apply the style that is given by MARCEL.

--- frontend/plugin.js
+++ frontend/plugin.js
@@ -73,6 +73,7 @@ class ZenikaFormations extends Marcel.Plugin {
             }
             this.displayList()
             this.displaySessionDetail()
+            this.updateSelectionColor()
         }
     }
 
@@ -87,19 +88,72 @@ class ZenikaFormations extends Marcel.Plugin {
         }
     }
 
+    changeBGColor(className, color) {
+        const el = document.getElementsByClassName(className)
+        let fc = "#191919"
+        if (tinycolor(color).isDark()) {
+            fc = "#f9f9f9"
+        }
+        
+        for(let i=0; i<el.length; i++) {
+            el[i].style.backgroundColor = color
+            el[i].style.color = fc
+        }
+    }
+
+    changeFontColor(className, color) {
+        const el = document.getElementsByClassName(className)
+        for(let i=0; i<el.length; i++) {
+            el[i].style.color = color
+        }
+    }
+
+    updateSelectionColor() {
+        const el = document.getElementsByClassName("selected")
+        const sc = tinycolor(this.props.stylevars["secondary-color"])
+        let comp = ""
+        if(sc.isDark()) {
+            comp = sc.lighten(65).toHexString()
+        } else {
+            comp = sc.darken(65).toHexString()
+        }
+        
+        for(let i=0; i<el.length; i++) {
+            el[i].style.backgroundColor = comp
+            el[i].style.color = this.props.stylevars["secondary-color"]
+        }
+    }
+
+    applyStyle(stylevars) {
+        const bg = stylevars["background-color"]
+        const pc = stylevars["primary-color"]
+        const sc = stylevars["secondary-color"]
+        const ff = stylevars["font-family"]
+        const body = document.getElementsByTagName("body")[0]
+        body.style.fontFamily = ff
+        body.style.backgroundColor = bg
+        this.changeBGColor("primary-bg", pc)
+        this.changeBGColor("secondary-bg", sc)
+        this.changeFontColor("primary-col", pc)
+        this.changeFontColor("secondary-col", sc)
+    }
+
     render() {
         const {
             agency,
             titleMessage,
-            displayTime
+            displayTime, 
+            stylevars
         } = this.props
         const listTitle = document.getElementById("list-title")
         listTitle.innerHTML = titleMessage.replace("{agency}", agency)
 
+        this.applyStyle(stylevars)
+
         if (this.intervalID !== null) {
             window.clearInterval(this.intervalID)
         }
-        this.intervalID = window.setInterval(_ => this.updateSelection(), displayTime * 1000)
+        this.intervalID = window.setInterval(() => this.updateSelection(), displayTime * 1000)
     }
 }
 
@@ -108,5 +162,11 @@ const instance = new ZenikaFormations()
 Marcel.Debug.changeProps({
     agency: "Nantes",
     titleMessage: "Next formation in {agency}",
-    displayTime: 5
+    displayTime: 5,
+    stylevars: {
+        "background-color": "#FFFFFF",
+        "primary-color": "rgb(202, 40, 40)", 
+        "secondary-color": "rgb(240, 240, 240)", 
+        "font-family": "Roboto"
+    }
 })
