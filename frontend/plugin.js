adb98c5b3dbc983047a9080e80d16e63ff391122
Maël NACCACHE <mael.naccache@zenika.com>
2018-02-07 10:14:36 +0100

    :art: Improving code style consistency.

--- frontend/plugin.js
+++ frontend/plugin.js
@@ -2,51 +2,51 @@ class ZenikaFormations extends Marcel.Plugin {
     constructor() {
         super({
             defaultProps: {
-                agency: '',
+                agency: "",
                 titleMessage: "Next formation in {agency}",
                 displayTime: 1
             },
         })
-        
-        this.formationList = document.getElementById('content')
+
+        this.formationList = document.getElementById("content")
         this.sessionList = []
         this.selectedIndex = 0
         this.urlFormationList = "http://localhost:8080/api/public/planned-sessions"
         this.intervalID = null
     }
-    
+
     getAllSessionsInAgency(agency) {
         return fetch(this.urlFormationList)
-        .then(response => response.json())
-        .then(formJson => formJson.plannedSessions.filter(e => e.agencyName === agency))
+            .then(response => response.json())
+            .then(formJson => formJson.plannedSessions.filter(e => e.agencyName === agency))
     }
 
     displayAllFuturSessions(agency) {
         this.getAllSessionsInAgency(agency)
-        .then(sessions => {
-            this.sessionList = sessions
-            this.displayList()
-        })
+            .then(sessions => {
+                this.sessionList = sessions
+                this.displayList()
+            })
     }
 
     displayList() {
         while (this.formationList.firstChild) {
-            this.formationList.removeChild(this.formationList.firstChild);
+            this.formationList.removeChild(this.formationList.firstChild)
         }
         this.sessionList.forEach((e, i) => {
             const formation = document.createElement("li")
-            if(i === this.selectedIndex) {
+            if (i === this.selectedIndex) {
                 formation.classList.add("selected")
             }
-            formation.innerHTML = "Formation " + e.trainingTitle + ", " + e.trainingSubTitle + " | " + 
-            e.startDate + " at " + e.agencyName
+            formation.innerHTML = "Formation " + e.trainingTitle + ", " + e.trainingSubTitle + " | " +
+                e.startDate + " at " + e.agencyName
             this.formationList.appendChild(formation)
         })
     }
-    
+
     updateSelection() {
-        if(this.sessionList !== undefined && this.sessionList.length !== 0) {
-            if(this.selectedIndex === this.sessionList.length) {
+        if (this.sessionList !== undefined && this.sessionList.length !== 0) {
+            if (this.selectedIndex === this.sessionList.length - 1) {
                 this.selectedIndex = 0
             } else {
                 this.selectedIndex += 1
@@ -54,26 +54,38 @@ class ZenikaFormations extends Marcel.Plugin {
             this.displayList()
         }
     }
-    
+
     propsDidChange(prevProps) {
-        const { agency, titleMessage, displayTime } = this.props
+        const {
+            agency,
+            titleMessage,
+            displayTime
+        } = this.props
         if (agency !== prevProps.agency) {
             this.displayAllFuturSessions(agency)
         }
     }
-    
+
     render() {
-        const { agency, titleMessage, displayTime } = this.props
+        const {
+            agency,
+            titleMessage,
+            displayTime
+        } = this.props
         const listTitle = document.getElementById("list-title")
         listTitle.innerHTML = titleMessage.replace("{agency}", agency)
 
-        if(this.intervalID !== null) {
+        if (this.intervalID !== null) {
             window.clearInterval(this.intervalID)
         }
-        this.intervalID = window.setInterval(a => this.updateSelection(), displayTime*1000)
+        this.intervalID = window.setInterval(_ => this.updateSelection(), displayTime * 1000)
     }
 }
 
 const instance = new ZenikaFormations()
 
-Marcel.Debug.changeProps({ agency: "Nantes", titleMessage: "Next formation in {agency}", displayTime: 1 })
+Marcel.Debug.changeProps({
+    agency: "Nantes",
+    titleMessage: "Next formation in {agency}",
+    displayTime: 1
+})
