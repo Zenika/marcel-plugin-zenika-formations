a26e280b2e5eea0f282a62552de4b990bcdf8e5b
Maël NACCACHE <mael.naccache@zenika.com>
2018-02-08 15:33:25 +0100

    :art: Use CSS variable to better handle the colors and font size and change the handling of style in the JS.

--- frontend/plugin.js
+++ frontend/plugin.js
@@ -3,8 +3,9 @@ class ZenikaFormations extends Marcel.Plugin {
         super({
             defaultProps: {
                 agency: "",
-                titleMessage: "Next formation in {agency}",
-                displayTime: 1
+                titleMessage: "Next formations in {agency}",
+                displayTime: 1,
+                locale: "fr"
             },
         })
 
@@ -31,36 +32,55 @@ class ZenikaFormations extends Marcel.Plugin {
     }
 
     displaySessionDetail() {
-        const [title, subtitle, trainer, date, summary] = [
+        const [title, subtitle, trainer, date, duration, summary] = [
             document.getElementById("title"),
             document.getElementById("subtitle"),
             document.getElementById("trainer"),
             document.getElementById("date"),
+            document.getElementById("duration"),
             document.getElementById("summary")
         ]
         const session = this.sessionList[this.selectedIndex]
-        title.innerHTML = session.trainingTitle
-        subtitle.innerHTML = session.trainingSubTitle
-        if(session.trainerFirstName !== null && session.trainerLastName !== null) {
-            trainer.innerHTML = session.trainerFirstName + " " + session.trainerLastName
+        title.innerText = session.trainingTitle
+        subtitle.innerText = session.trainingSubTitle
+        if(session.trainerFirstName && session.trainerLastName) {
+            trainer.innerText = "Par " + session.trainerFirstName + " " + session.trainerLastName + " "
         }
-        const endDate = new Date(session.startDate)
-        endDate.setDate(endDate.getDate() + session.trainingDays)
-        date.innerHTML = session.startDate + " - " + endDate.getFullYear() + "-" + endDate.getMonth() + "-" + endDate.getDay()
+        const endDate = moment(session.startDate, "YYYY-MM-DD").format("LL")
+        date.innerText = "le " + endDate + ". "
+
+        duration.innerText = "Dure: " + session.trainingDays + (session.trainingDays > 1 ? " jours." : "jour.")
+        
+        // TODO: Remove the HTML in the description and use innerText
         summary.innerHTML = session.training.description
     }
 
     displayList() {
-        while (this.formationList.firstChild) {
-            this.formationList.removeChild(this.formationList.firstChild)
-        }
-        this.sessionList.forEach((e, i) => {
-            const formation = document.createElement("li")
-            if (i === this.selectedIndex) {
-                formation.classList.add("selected")
+        this.formationList.innerHTML = ""
+        this.sessionList.forEach((formation, index) => {
+            const item = document.createElement("div")
+            item.classList.add("flex-container")
+            item.classList.add("list-item")
+            if (index === this.selectedIndex) {
+                item.classList.add("selected")
             }
-            formation.innerHTML = e.startDate + ": " + e.trainingTitle
-            this.formationList.appendChild(formation)
+            if (index === 0) {
+                item.classList.add("top")
+            }
+
+            const name = document.createElement("div")
+            name.style.flex = 1
+            const date = document.createElement("div")
+            date.style.flex = 1
+
+            const startDate = moment(formation.startDate, "YYYY-MM-DD").format("dddd DD MMMM")
+            name.innerText = formation.trainingTitle
+            date.classList.add("date-info")
+            date.classList.add("text-right")
+            date.innerText = startDate
+            item.appendChild(name)
+            item.appendChild(date)
+            this.formationList.appendChild(item)
         })
     }
 
@@ -74,17 +94,6 @@ class ZenikaFormations extends Marcel.Plugin {
             this.displayList()
             this.displaySessionDetail()
             this.updateSelectionColor()
-        }
-    }
-
-    propsDidChange(prevProps) {
-        const {
-            agency,
-            titleMessage,
-            displayTime
-        } = this.props
-        if (agency !== prevProps.agency) {
-            this.displayAllFuturSessions(agency)
         }
     }
 
@@ -109,6 +118,7 @@ class ZenikaFormations extends Marcel.Plugin {
     }
 
     updateSelectionColor() {
+        /*
         const el = document.getElementsByClassName("selected")
         const sc = tinycolor(this.props.stylevars["secondary-color"])
         let comp = ""
@@ -122,9 +132,14 @@ class ZenikaFormations extends Marcel.Plugin {
             el[i].style.backgroundColor = comp
             el[i].style.color = this.props.stylevars["secondary-color"]
         }
+        */
     }
 
     applyStyle(stylevars) {
+        const {
+            "background-color": backgroundColor
+        } = stylevars
+
         const bg = stylevars["background-color"]
         const pc = stylevars["primary-color"]
         const sc = stylevars["secondary-color"]
@@ -136,6 +151,21 @@ class ZenikaFormations extends Marcel.Plugin {
         this.changeBGColor("secondary-bg", sc)
         this.changeFontColor("primary-col", pc)
         this.changeFontColor("secondary-col", sc)
+    }
+
+    propsDidChange(prevProps) {
+        const {
+            agency,
+            titleMessage,
+            displayTime,
+            locale
+        } = this.props
+        if (agency !== prevProps.agency) {
+            this.displayAllFuturSessions(agency)
+        }
+        if (locale !== prevProps.locale) {
+            moment.locale(locale)
+        }
     }
 
     render() {
@@ -163,6 +193,7 @@ Marcel.Debug.changeProps({
     agency: "Nantes",
     titleMessage: "Next formation in {agency}",
     displayTime: 5,
+    locale: "fr",
     stylevars: {
         "background-color": "#FFFFFF",
         "primary-color": "rgb(202, 40, 40)", 
